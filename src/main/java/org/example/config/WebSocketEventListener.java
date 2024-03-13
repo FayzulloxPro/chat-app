56f404b878610f938236576dadd059f2df30bb55
Fayzullox <qahramonovf26@gmail.com>
2024-03-13 18:52:23 +0500

    first commit

---
+++ src/main/java/org/example/config/WebSocketEventListener.java
@@ -0,0 +1,23 @@
+package org.example.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.apachecommons.CommonsLog;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(
+            SessionDisconnectEvent event
+    ){
+        //TODO
+    }
+
+
+}
